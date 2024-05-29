cf3d1ee251b6482536b758daab30ae6511b8cced
CoenAntens <jacobusantens@gmail.com>
2024-05-29 20:28:53 +0200

    no ChatBot anymore

--- script.js
+++ script.js
@@ -50,22 +50,3 @@ toggleAdditionalInformation.addEventListener('click', () => {
 toggleReviews.addEventListener('click', () => {
 	contentReviews.classList.toggle('hidden');
 });
-
-
-(function (w, d, s, o, f, js, fjs) {
-	w["botsonic_widget"] = o;
-	w[o] =
-	  w[o] ||
-	  function () {
-		(w[o].q = w[o].q || []).push(arguments);
-	  };
-	(js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-	js.id = o;
-	js.src = f;
-	js.async = 1;
-	fjs.parentNode.insertBefore(js, fjs);
-  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
-  Botsonic("init", {
-	serviceBaseUrl: "https://api-azure.botsonic.ai",
-	token: "6734f479-c105-4fe0-89fd-abe696559f29",
-  });
